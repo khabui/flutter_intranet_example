2b435e2fe1d9dc53525b9c447e4cb4d0ff2ea2db
Kha Bui <duc.kha05@gmail.com>
2020-07-19 23:17:49 +0700

    Added MainScreen

---
+++ lib/ui/screens/main_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../resources/colors.dart';
+import '../../resources/icons.dart';
+import '../view_models/bottom_navigation_bar_provider.dart';
+import 'contacts_screen.dart';
+
+class MainScreen extends StatelessWidget {
+  final _tabItems = [
+    BottomNavigationBarItem(
+      icon: Icon(AppIcons.homeIcon),
+      title: const Text('Home'),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(AppIcons.meetingIcon),
+      title: const Text('Meeting'),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(AppIcons.teamIcon),
+      title: const Text(
+        'Team',
+      ),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(AppIcons.contactIcon),
+      title: const Text('Contact'),
+    ),
+  ];
+
+  final _currentTab = [
+    const ContactsScreen(
+      title: 'Contacts',
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<BottomNavigationBarProvider>(context);
+
+    return Scaffold(
+      body: _currentTab[provider.currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: provider.currentIndex,
+        onTap: (index) {
+          provider.currentIndex = index;
+        },
+        selectedItemColor: AppColors.blackColor,
+        unselectedItemColor: AppColors.greyColor,
+        items: _tabItems,
+      ),
+    );
+  }
+}
