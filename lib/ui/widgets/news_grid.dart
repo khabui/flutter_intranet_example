b29b064c125d0a71e1ac2ee2c9183f76581e1cf1
Kha Bui <duc.kha05@gmail.com>
2020-07-20 10:24:55 +0700

    Updated Navigator to ArticleScreen

--- lib/ui/widgets/news_grid.dart
+++ lib/ui/widgets/news_grid.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_intranet_example/resources/text_theme.dart';
 
+import '../../resources/text_theme.dart';
+import '../../resources/values.dart';
+import '../screens/article_screen.dart';
 import '../view_models/news_article_view_model.dart';
 import 'rounded_corner_image.dart';
 
@@ -9,6 +11,11 @@ class NewsGrid extends StatelessWidget {
   final List<NewsArticleViewModel> articles;
 
   const NewsGrid({@required this.articles});
+
+  void _showArticle(BuildContext context, NewsArticleViewModel vm) {
+    Navigator.of(context).pushNamed(Routes.article,
+        arguments: ArticleScreenArguments(article: vm));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,23 +26,28 @@ class NewsGrid extends StatelessWidget {
       itemCount: articles.length,
       itemBuilder: (context, index) {
         final article = articles[index];
-        return GridTile(
-          footer: Container(
-            margin: const EdgeInsets.symmetric(horizontal: 16.0,),
-            child: Text(
-              article.title,
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-              style: textTheme(context).bodyMedium.bold,
+        return GestureDetector(
+          onTap: () => _showArticle(context, article),
+          child: GridTile(
+            footer: Container(
+              margin: const EdgeInsets.symmetric(
+                horizontal: 16.0,
+              ),
+              child: Text(
+                article.title,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: textTheme(context).bodyMedium.bold,
+              ),
             ),
-          ),
-          child: Container(
-            margin: const EdgeInsets.symmetric(
-              horizontal: 12.0,
-              vertical: 20.0,
-            ),
-            child: RoundedCornerImageWidget(
-              imageUrl: article.urlToImages,
+            child: Container(
+              margin: const EdgeInsets.symmetric(
+                horizontal: 12.0,
+                vertical: 20.0,
+              ),
+              child: RoundedCornerImageWidget(
+                imageUrl: article.urlToImages,
+              ),
             ),
           ),
         );
