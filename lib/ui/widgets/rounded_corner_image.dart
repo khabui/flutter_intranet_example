f977de51707f15f0b7154ca9f4e0750dfeaf296e
Kha Bui <duc.kha05@gmail.com>
2020-09-07 16:41:28 +0700

    Handled null imageUrl

--- lib/ui/widgets/rounded_corner_image.dart
+++ lib/ui/widgets/rounded_corner_image.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_intranet_example/resources/icons.dart';
 
 class RoundedCornerImageWidget extends StatelessWidget {
   final String imageUrl;
@@ -9,27 +10,29 @@ class RoundedCornerImageWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CachedNetworkImage(
-      imageUrl: imageUrl,
-      imageBuilder: (context, imageProvider) {
-        return Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8.0),
-            image: DecorationImage(
-              image: imageProvider,
-              fit: BoxFit.cover,
-            ),
-          ),
-        );
-      },
-      placeholder: (context, url) {
-        return const Center(
-          child: CircularProgressIndicator(),
-        );
-      },
-      errorWidget: (context, url, error) {
-        return Container();
-      },
-    );
+    return imageUrl != null
+        ? CachedNetworkImage(
+            imageUrl: imageUrl,
+            imageBuilder: (context, imageProvider) {
+              return Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  image: DecorationImage(
+                    image: imageProvider,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              );
+            },
+            placeholder: (context, url) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+            errorWidget: (context, url, error) {
+              return const Icon(AppIcons.errorIcon);
+            },
+          )
+        : const Icon(AppIcons.errorIcon);
   }
 }
